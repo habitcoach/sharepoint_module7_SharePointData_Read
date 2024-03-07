e76bfdd5a770c737eab7ccc9130729727d204862
habitcoach <the_habit_coach@outlook.com>
2024-03-07 21:55:20 +0530

    first commit

---
+++ src/webparts/spFxHttpClientDemo/components/ISpFxHttpClientDemoProps.ts
@@ -0,0 +1,13 @@
+import {
+  ButtonClickedCallback,
+  ICountryListItem
+} from '../../../models';
+
+export interface ISpFxHttpClientDemoProps {
+  spListItems: ICountryListItem[];
+  onGetListItems: ButtonClickedCallback;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
